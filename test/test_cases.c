4dac9918e656f91a80b8a38d2e7ace50a20e80a0
Mike Brockus <michaelbrockus@gmail.com>
2022-02-01 00:53:19 -0600

    write test cases

--- test/test_cases.c
+++ test/test_cases.c
@@ -9,44 +9,141 @@
 // project since its important to test once implementation against a set
 // of common test cases
 //
-#include "miok/package.h"
+#include "miok/dqueue.h"
 #include <unity.h>
+
+DeQueueOf *queue;
 
 //
 //  project setup teardown functions if needed
 //
 void setUp(void)
 {
-    // TODO.
+    queue = miok_dqueue_create();
 } // end of function setUp
 
 void tearDown(void)
 {
-    // TODO.
+    miok_dqueue_erase(&queue);
 } // end of function tearDown
 
 //
 // list of all the test cases for this project
 //
-static void test_simpleAssertTrue(void)
+static void test_miok_dqueue_push_start(void)
 {
-    TEST_ASSERT_TRUE(1);
+    TEST_ASSERT_NOT_NULL(queue);
+    miok_dqueue_push_start(queue, "2021");
+    TEST_ASSERT_EQUAL_STRING("2021", miok_dqueue_peek_start(queue));
+
+    miok_dqueue_push_start(queue, "2077");
+    miok_dqueue_push_start(queue, "2010");
+    TEST_ASSERT_EQUAL_STRING("2010", miok_dqueue_peek_start(queue));
 } // end of test case
 
-static void test_simpleAssertNull(void)
+static void test_miok_dqueue_push_start_with_nullptr(void)
 {
-    TEST_ASSERT_NULL(NULL);
+    miok_dqueue_push_start(NULL, "2021");
+    TEST_ASSERT_EQUAL_STRING(NULL, miok_dqueue_peek_start(queue));
+
+    miok_dqueue_push_start(NULL, "2077");
+    miok_dqueue_push_start(NULL, "2010");
+    TEST_ASSERT_EQUAL_STRING(NULL, miok_dqueue_peek_start(queue));
 } // end of test case
 
-static void test_simpleAssertCompare(void)
+static void test_miok_dqueue_push_end(void)
 {
-    int dummy = 3;
-    TEST_ASSERT_EQUAL_INT(3, dummy);
+    TEST_ASSERT_NOT_NULL(queue);
+    miok_dqueue_push_end(queue, "2021");
+    TEST_ASSERT_EQUAL_STRING("2021", miok_dqueue_peek_start(queue));
+    TEST_ASSERT_EQUAL_STRING("2021", miok_dqueue_peek_end(queue));
+
+    miok_dqueue_push_end(queue, "2077");
+    miok_dqueue_push_end(queue, "2010");
+    TEST_ASSERT_EQUAL_STRING("2021", miok_dqueue_peek_start(queue));
+    TEST_ASSERT_EQUAL_STRING("2010", miok_dqueue_peek_end(queue));
 } // end of test case
 
-static void test_simpleAssertCall(void)
+static void test_miok_dqueue_push_end_with_nullptr(void)
 {
-    TEST_ASSERT_EQUAL_STRING("Hello, C Developer.", greet());
+    miok_dqueue_push_end(NULL, "2021");
+    TEST_ASSERT_EQUAL_STRING(NULL, miok_dqueue_peek_start(queue));
+    TEST_ASSERT_EQUAL_STRING(NULL, miok_dqueue_peek_end(queue));
+
+    miok_dqueue_push_end(NULL, "2077");
+    miok_dqueue_push_end(NULL, "2010");
+    TEST_ASSERT_EQUAL_STRING(NULL, miok_dqueue_peek_start(queue));
+    TEST_ASSERT_EQUAL_STRING(NULL, miok_dqueue_peek_end(queue));
+} // end of test case
+
+static void test_miok_dqueue_pop_start(void)
+{
+    TEST_ASSERT_NOT_NULL(queue);
+    miok_dqueue_push_start(queue, "red chicken");
+    miok_dqueue_push_start(queue, "blue chicken");
+    miok_dqueue_push_start(queue, "black chicken");
+    miok_dqueue_push_start(queue, "green chicken");
+    miok_dqueue_push_start(queue, "yellow chicken");
+
+    TEST_ASSERT_EQUAL_STRING("yellow chicken", miok_dqueue_peek_start(queue));
+    miok_dqueue_pop_start(queue);
+    TEST_ASSERT_EQUAL_STRING("green chicken", miok_dqueue_peek_start(queue));
+    miok_dqueue_pop_start(queue);
+    TEST_ASSERT_EQUAL_STRING("black chicken", miok_dqueue_peek_start(queue));
+
+} // end of test case
+
+static void test_miok_dqueue_pop_end(void)
+{
+    TEST_ASSERT_NOT_NULL(queue);
+    miok_dqueue_push_end(queue, "red chicken");
+    miok_dqueue_push_end(queue, "blue chicken");
+    miok_dqueue_push_end(queue, "black chicken");
+    miok_dqueue_push_end(queue, "green chicken");
+    miok_dqueue_push_end(queue, "yellow chicken");
+
+    TEST_ASSERT_EQUAL_STRING("red chicken", miok_dqueue_peek_start(queue));
+    miok_dqueue_pop_end(queue);
+    TEST_ASSERT_EQUAL_STRING("red chicken", miok_dqueue_peek_start(queue));
+    miok_dqueue_pop_end(queue);
+    TEST_ASSERT_EQUAL_STRING("red chicken", miok_dqueue_peek_start(queue));
+
+} // end of test case
+
+static void test_miok_dqueue_its_empty(void)
+{
+    TEST_ASSERT_NOT_NULL(queue);
+    TEST_ASSERT_TRUE(miok_dqueue_its_empty(queue));
+    miok_dqueue_push_start(queue, "red chicken");
+    TEST_ASSERT_FALSE(miok_dqueue_its_empty(queue));
+
+    TEST_ASSERT_EQUAL_STRING("red chicken", miok_dqueue_peek_start(queue));
+} // end of test case
+
+static void test_miok_dqueue_its_empty_with_nullptr(void)
+{
+    miok_dqueue_push_start(NULL, "red chicken");
+    TEST_ASSERT_TRUE(miok_dqueue_its_empty(queue));
+
+    TEST_ASSERT_EQUAL_STRING(NULL, miok_dqueue_peek_start(queue));
+} // end of test case
+
+static void test_miok_dqueue_not_empty(void)
+{
+    TEST_ASSERT_NOT_NULL(queue);
+    TEST_ASSERT_FALSE(miok_dqueue_not_empty(queue));
+    miok_dqueue_push_start(queue, "red chicken");
+    TEST_ASSERT_TRUE(miok_dqueue_not_empty(queue));
+
+    TEST_ASSERT_EQUAL_STRING("red chicken", miok_dqueue_peek_start(queue));
+} // end of test case
+
+static void test_miok_dqueue_not_empty_with_nullptr(void)
+{
+    miok_dqueue_push_start(NULL, "red chicken");
+    TEST_ASSERT_FALSE(miok_dqueue_not_empty(queue));
+
+    TEST_ASSERT_EQUAL_STRING(NULL, miok_dqueue_peek_start(queue));
 } // end of test case
 
 //
@@ -56,10 +153,16 @@ int main(void)
 {
     UNITY_BEGIN();
 
-    RUN_TEST(test_simpleAssertTrue);
-    RUN_TEST(test_simpleAssertNull);
-    RUN_TEST(test_simpleAssertCall);
-    RUN_TEST(test_simpleAssertCompare);
+    RUN_TEST(test_miok_dqueue_push_start);
+    RUN_TEST(test_miok_dqueue_push_end);
+    RUN_TEST(test_miok_dqueue_pop_start);
+    RUN_TEST(test_miok_dqueue_pop_end);
+    RUN_TEST(test_miok_dqueue_its_empty);
+    RUN_TEST(test_miok_dqueue_not_empty);
+    RUN_TEST(test_miok_dqueue_push_start_with_nullptr);
+    RUN_TEST(test_miok_dqueue_push_end_with_nullptr);
+    RUN_TEST(test_miok_dqueue_its_empty_with_nullptr);
+    RUN_TEST(test_miok_dqueue_not_empty_with_nullptr);
 
     return UNITY_END();
 } // end of function main
